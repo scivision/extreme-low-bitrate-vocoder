f37e187b2c6b86efd4cd3a8b201dc8c4d6ef6d63
scivision <scivision@users.noreply.github.com>
2025-11-09 14:19:18 -0500

    unused, optional args

--- GenerateLPCcoeff.m
+++ GenerateLPCcoeff.m
@@ -1,5 +1,5 @@
 function [LPCpoles, G, FFerr, formantFreqs] = GenerateLPCcoeff(...
-    WindowedAudio,modelOrder,nFrames,WinL,FrameL,PreEmphAlpha,Fs,SnapInd,LiftLowQueRecp,LiftHighQueRecp,file,pMode,gMode,diag)
+    WindowedAudio,modelOrder,nFrames,WinL,FrameL,PreEmphAlpha,Fs,SnapInd,LiftLowQueRecp,LiftHighQueRecp,file,pMode,diag)
 %MyLPC(pm,data,diag)
 %diag: true to enable lifter output plot
 
@@ -16,82 +16,83 @@ for i = 1:nFrames
 if ~all(WindowedAudio(:,i) ==0)
   % pre-emphasize speech for LPC coding +6dB per octave
   % by introducing a zero near w=0. This simulates lips!
-    num = [1 -PreEmphAlpha];
-    den = 1;
-WindowedAudio(:,i) = filter(num,den,WindowedAudio(:,i));
-%WindowedAudio(:,i) = filter([1 -0.25],den,WindowedAudio(:,i)); %cascade additional pre-emphasis
-Cwind(:,i) = cceps(WindowedAudio(:,i));
-switch pMode
+  num = [1 -PreEmphAlpha];
+  den = 1;
+  WindowedAudio(:,i) = filter(num,den,WindowedAudio(:,i));
+  %WindowedAudio(:,i) = filter([1 -0.25],den,WindowedAudio(:,i)); %cascade additional pre-emphasis
+  Cwind(:,i) = cceps(WindowedAudio(:,i));
+  switch pMode
     case 'vtract'
-CLift(:,i) = lifter(Cwind(:,i),Fs,LiftLowQueRecp,'lowcut');
-LifteredVocalTract = icceps(CLift(:,i));
-%xPreemph = filter([1 -0.65],den,xPreemph);  % cascade filter
+      CLift(:,i) = lifter(Cwind(:,i),Fs,LiftLowQueRecp,'lowcut');
+      LifteredVocalTract = icceps(CLift(:,i));
+      % xPreemph = filter([1 -0.65],den, xPreemph);  % cascade filter
     case 'glottal'
-        CLift(:,i) = lifter(Cwind,LiftHighQueRecp,'hicut');
-        xPreemph = icceps(CLift(:,i)); %no preemph for glottal
+      CLift(:,i) = lifter(Cwind,LiftHighQueRecp,'hicut');
+      % xPreemph = icceps(CLift(:,i)); %no preemph for glottal
     otherwise, error(['unrecognized option',pMode,'in MyLPC'])
-end
+  end
 
 
 %% levinson-durbin recursion
-    [LPCpoles(:,i),G(i)] = LevDurb(LifteredVocalTract,WinL,modelOrder);
-      %[LPCpoles(:,i),G(i)] = lpc(xPreemph,modelOrder);
-      G(i) = sqrt(G(i));
+  [LPCpoles(:,i),G(i)] = LevDurb(LifteredVocalTract,WinL,modelOrder);
+  %[LPCpoles(:,i),G(i)] = lpc(xPreemph,modelOrder);
+  G(i) = sqrt(G(i));
 
 
-    switch pMode
-        case 'vtract'
-   %% compute prediction errors
-    FFerr(:,i) = filter(LPCpoles(:,i),G(i),WindowedAudio(:,i));
-    %% find pole center frequencies
-    LPCroots = roots(LPCpoles(:,i));
-    LPCroots = LPCroots(imag(LPCroots)>0); %select formants on w = 0..pi/2
-   % to find the location of a root, find its angle w.r.t. w = 0 using
-   % atan2.
- formantFreqs(1:length(LPCroots),i) = atan2(imag(LPCroots),real(LPCroots));
+  switch pMode
+    case 'vtract'
+%% compute prediction errors
+     FFerr(:,i) = filter(LPCpoles(:,i),G(i),WindowedAudio(:,i));
+%% find pole center frequencies
+     LPCroots = roots(LPCpoles(:,i));
+     LPCroots = LPCroots(imag(LPCroots)>0); %select formants on w = 0..pi/2
+   % to find the location of a root, find its angle w.r.t. w = 0 using atan2.
+     formantFreqs(1:length(LPCroots),i) = atan2(imag(LPCroots),real(LPCroots));
 
 
 
 
-        case 'glottal'
-            %formantFreqs(1,i) = NaN;
-    end
-       else FFerr(:,i) = nan;
-   end
+  case 'glottal'
+      %formantFreqs(1,i) = NaN;
+  end
+else
+  FFerr(:,i) = nan;
+end
+
 end
 formantFreqs = formantFreqs.*Fs/(2*pi); % since f = w/(2pi) and take Fs into account
 % remove NaNs from zero data input frames
-    G(isnan(G))=eps;
-    %% put unstable poles to stable: 1/conj(p)
-     %LPCpoles(abs(LPCpoles)>1) = -1./conj(LPCpoles(abs(LPCpoles)>1));
-     % need to add all-pole gain scaling!
-    Lunstable = sum(abs(LPCpoles)>1);
-    iUnstable = find(Lunstable>0);
-    Lunstable = sum(Lunstable);
-    if Lunstable
-      disp(['there were ',int2str(Lunstable),' poles detected outside unit circle in initial LPC model of voice tract.'])
-      disp(['The unstable poles were in frames: ',int2str(iUnstable),])
-    end
+G(isnan(G))=eps;
+%% put unstable poles to stable: 1/conj(p)
+ %LPCpoles(abs(LPCpoles)>1) = -1./conj(LPCpoles(abs(LPCpoles)>1));
+ % need to add all-pole gain scaling!
+Lunstable = sum(abs(LPCpoles)>1);
+iUnstable = find(Lunstable>0);
+Lunstable = sum(Lunstable);
+if Lunstable
+  disp(['there were ',int2str(Lunstable),' poles detected outside unit circle in initial LPC model of voice tract.'])
+  disp(['The unstable poles were in frames: ',int2str(iUnstable),])
+end
 %% diag plot
 if diag
   txt = "1/Quefrency cutoff: " + num2str(LiftLowQueRecp,'%03.1f') + " [s]   File: " + file;
-    %plot
-    figure,
-    zplane(G(SnapInd),LPCpoles(:,SnapInd)),
-    title({['Poles/zeros of LPC Vocal Tract model @ Time= ',num2str(SnapInd*FrameL/Fs),' sec.']; txt})
+
+  figure,
+  zplane(G(SnapInd),LPCpoles(:,SnapInd)),
+  title({['Poles/zeros of LPC Vocal Tract model @ Time= ',num2str(SnapInd*FrameL/Fs),' sec.']; txt})
 
 
-StartAx=Fs/1000;
-StopAx=Fs/50;
-q=(StartAx:StopAx)/Fs;
-figure
-stem(q,CLift(StartAx:StopAx,SnapInd),'displayname','Liftered Cepstrum')
-hold on
-stem(q,Cwind(StartAx:StopAx,SnapInd),'r','displayname','un-liftered cepstrum')
-title({['Liftered Cepstrum into LPC Vocal Tract predictor at Time =  ',num2str(SnapInd*FrameL/Fs), ' sec.'], txt})
-xlabel('Quefrency [s]')
-ylabel('Cepstral Amplitude (relative)')
-legend('show')
+  StartAx=Fs/1000;
+  StopAx=Fs/50;
+  q=(StartAx:StopAx)/Fs;
+  figure
+  stem(q,CLift(StartAx:StopAx,SnapInd),'displayname','Liftered Cepstrum')
+  hold on
+  stem(q,Cwind(StartAx:StopAx,SnapInd),'r','displayname','un-liftered cepstrum')
+  title({['Liftered Cepstrum into LPC Vocal Tract predictor at Time =  ',num2str(SnapInd*FrameL/Fs), ' sec.'], txt})
+  xlabel('Quefrency [s]')
+  ylabel('Cepstral Amplitude (relative)')
+  legend('show')
 end
 
 end
--- LPCceps.m
+++ LPCceps.m
@@ -1,50 +1,47 @@
-function [LPCcep, CepPoles, TractG, CepsFFerr] = LPCceps(WindowedAudio,TractG,TractPoles,fundExcite,KeepCeps,LPCerr,nFrames,WinL,FrameL,Ns,Fs,file,glottMode,modelOrder,SI,diag)
+function [LPCcep, CepPoles, CepsFFerr] = LPCceps(WindowedAudio,TractG,TractPoles,KeepCeps,nFrames,WinL,FrameL,Fs,file,modelOrder,SI,diag)
+
 LPCcep = zeros(modelOrder+1,nFrames);
 
-
-
 for i = 1:nFrames
-    if ~isnan(TractPoles(:,i))
-        % Convert LPC to cepstral coefficients using custom function
-        LPCcep(:,i) = lpc2cepstral(TractPoles(2:end,i), TractG(i)); % Convert LPC to CC.
-    end
+  if ~isnan(TractPoles(:,i))
+    % Convert LPC to cepstral coefficients
+    LPCcep(:,i) = lpc2cepstral(TractPoles(2:end,i), TractG(i));
+  end
 end
-% remove low values of LPC cepstrum, as they imply "less important"
-% quefrency content
+% remove low values of LPC cepstrum, as they imply "less important" quefrency content
 LPCcep(KeepCeps+1:end,:) = 0;
-%%
-for i = 1:nFrames
-    %Convert cepstral coefficients back to LPC using custom function
-    CepPoles(:,i) = cepstral2lpc(LPCcep(:,i));
-    [Hejw(:,i), w(:,i) ] = freqz(TractG(i),CepPoles(:,i),WinL,'whole',Fs);
-%% compute feedforward prediction errors
-    CepsFFerr(:,i) = filter(CepPoles(:,i),TractG(i),WindowedAudio(:,i));
-end
 
-%%
 if diag
 
-figure
-Y = 20*log10(abs(Hejw));
-title(['Cepstral compressed frequency response at t = ',num2str(SI*FrameL/Fs)])
-xlabel('Frequency [Hz]')
-ylabel('Cepstral Liftered LPC Magnitude Reponse (dB Relative)')
-plot(w(1:fix(WinL/2),SI),Y(1:fix(WinL/2),SI),'linewidth',2,'color','r','displayname','LPC Spectrum');
-
-for i = 1: numel(CepPoles)
-    if abs(CepPoles(i))>1
-       % insert code to correct here
+  for i = 1:nFrames
+    % Convert cepstral coefficients back to LPC using custom function
+    CepPoles(:,i) = cepstral2lpc(LPCcep(:,i));
+    [Hejw(:,i), w(:,i) ] = freqz(TractG(i),CepPoles(:,i),WinL,'whole',Fs);
+    % compute feedforward prediction errors
+    if nargout > 2
+      CepsFFerr(:,i) = filter(CepPoles(:,i),TractG(i),WindowedAudio(:,i));
     end
-end
+  end
 
-%     [xCep xCepW Excite] = regenerateSignalFromLPCcoeff(...
-%     ones(1,nFrames),CepPoles,fundExcite,LPCerr,Ns,Fs,...
-%     FrameL,WinL,nFrames,glottMode,SI,'Cepstral Processor',diag);
-%
-stem(mean(LPCcep,2))
-ylim([-1 1])
-title("MEAN of LPC-cepstrum " + file)
-xlabel('quefrency(n)')
+%% diagnostic plot
+
+  figure
+  Y = 20*log10(abs(Hejw));
+  title(['Cepstral compressed frequency response at t = ',num2str(SI*FrameL/Fs)])
+  xlabel('Frequency [Hz]')
+  ylabel('Cepstral Liftered LPC Magnitude Reponse (dB Relative)')
+  plot(w(1:fix(WinL/2),SI),Y(1:fix(WinL/2),SI),'linewidth',2,'color','r','displayname','LPC Spectrum');
+
+  for i = 1: numel(CepPoles)
+    if abs(CepPoles(i))>1
+      % FIXME: insert code to correct here
+    end
+  end
+
+  stem(mean(LPCcep,2))
+  ylim([-1 1])
+  title("MEAN of LPC-cepstrum " + file)
+  xlabel('quefrency(n)')
 
 end
 
