443cfa26291008c1223ce03e96985b451820fac4
scivision <scivision@users.noreply.github.com>
2025-11-09 14:41:25 -0500

    ci

---
+++ buildfile.m
@@ -0,0 +1,54 @@
+function plan = buildfile
+
+plan = buildplan(localfunctions);
+
+pkgDir = fullfile(plan.RootFolder);
+test_root = fullfile(plan.RootFolder, 'test');
+reportDir = fullfile(plan.RootFolder, 'reports');
+if ~isfolder(reportDir)
+  mkdir(reportDir);
+end
+
+if isMATLABReleaseOlderThan('R2023a')
+  plan('test') = matlab.buildtool.Task('Actions', @(test_root) legacy_test(test_root));
+else
+  plan('test') =  matlab.buildtool.tasks.TestTask(test_root, SourceFiles=pkgDir);
+end
+
+if ~isMATLABReleaseOlderThan('R2024a')
+
+  coverageReport = fullfile(reportDir, 'coverage-report.html');
+  try
+    report = matlabtest.plugins.codecoverage.StandaloneReport(coverageReport);
+  catch
+    report = coverageReport;
+  end
+  plan('coverage') = plan('test').addCodeCoverage(report);
+  plan('coverage').DisableIncremental = true;
+end
+
+end
+
+
+function legacy_test(~, test_root)
+r = runtests(test_root);
+
+assert(~isempty(r), 'No tests were run')
+assertSuccess(r)
+end
+
+
+
+function checkTask(context)
+root = context.Plan.RootFolder;
+
+c = codeIssues(root, IncludeSubfolders=true);
+
+if isempty(c.Issues)
+  fprintf('%d files checked OK with %s under %s\n', numel(c.Files), c.Release, root)
+else
+  disp(c.Issues)
+  error("Errors found in " + join(c.Issues.Location, newline))
+end
+
+end
