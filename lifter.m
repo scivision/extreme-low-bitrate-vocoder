bd23b57fbc3aa02b805b41b07da81a931b56b1e7
scivision <scivision@users.noreply.github.com>
2025-11-09 13:39:57 -0500

    lint

--- lifter.m
+++ lifter.m
@@ -1,4 +1,4 @@
-function xLift = lifter(xHat, Fs,Nq, select,diag)
+function xLift = lifter(xHat, Fs,Nq, select)
 
 %xHat = complex cepstrum
 %NL = quefrency cutoff sample index
@@ -11,10 +11,8 @@ nNL = round(Fs/Nq*2); % index of cutoff quefrency
 xLift = xHat;
 % do low-time or high-time liftering
 switch select
-    case 'lowcut'
-    xLift(Lcent-nNL+1 : Lcent + nNL - 1) = 0;
-    case 'hicut'
-    xLift([1:Lcent-nNL+1 , Lcent + nNL - 1:end]) = 0;
-    otherwise error('Improper select input to lifter.m')
+    case 'lowcut', xLift(Lcent-nNL+1 : Lcent + nNL - 1) = 0;
+    case 'hicut', xLift([1:Lcent-nNL+1 , Lcent + nNL - 1:end]) = 0;
+    otherwise, error('Improper select input to lifter.m')
 end
 
